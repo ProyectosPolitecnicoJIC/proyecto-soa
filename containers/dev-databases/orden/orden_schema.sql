c0c32ffeab8122ac33207653d4fca6d2462f542d
Geffry Alejandro Ospina Atehortua <geffry.ospina@gmail.com>
2025-05-20 10:29:39 -0500

    Se corrige uno de los scripts de base de datos de orden

--- containers/dev-databases/orden/orden_schema.sql
+++ containers/dev-databases/orden/orden_schema.sql
@@ -2,8 +2,8 @@ CREATE TABLE ordenes (
     id INT PRIMARY KEY,
     id_usuario INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-)
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 CREATE TABLE ordenes_materiales (
     id INT PRIMARY KEY,
@@ -12,8 +12,8 @@ CREATE TABLE ordenes_materiales (
     cantidad INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_orden) REFERENCES ordenes(id),
-)
+    FOREIGN KEY (id_orden) REFERENCES ordenes(id)
+);
 
 -- la tabla de ordenes se relaciona con la tabla de ordenes_materiales por el id_orden
 -- la tabla de ordenes_materiales se relaciona con la tabla de materiales por el id_material
